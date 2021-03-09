b9f79ffc339fc18db5c11cded8ee8d8c911578f0
V. Claire Olmstead <viol5229@colorado.edu>
2021-03-08 17:04:55 -0800

    add content to pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <>
-      <h1>Footer</h1>
+      <p className="footer__p">
+        ©2020 Liberty 3.0 LLC \
+        <a href="mailto:hello@projectliberty.io">Contact</a> \
+        <Link to="/privacy-policy">Privacy Policy</Link> \
+        <a
+          href="https://www.unfinishedlabs.io"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Unfinished Labs
+        </a>
+      </p>
     </>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
 
 const Header = () => {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <Link to="/">
+      <img src={logo} alt="brand-logo" width={200} />
+    </Link>
   );
 };
 
