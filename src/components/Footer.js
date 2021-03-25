6e9776215f27e269239e54b8c89b99abc842371f
V. Claire Olmstead <43625033+claireolmstead@users.noreply.github.com>
2021-03-24 17:10:37 -0700

    footer (#15)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../images/logoDark.png";
 
 const Footer = () => {
   return (
     <div className="Footer__block" data-aos-offset="0" data-aos="fade">
-      <span>©2020 Liberty 3.0 LLC</span>
-      <span className="Footer__textSeparator">/</span>
-      <a className="Footer__textLink" href="mailto:hello@projectliberty.io">
-        Contact
-      </a>
-      <span className="Footer__textSeparator">/</span>
-      <Link className="Footer__textLink" to="/privacy-policy">
-        Privacy Policy
-      </Link>
-      <span className="Footer__textSeparator">/</span>
-      <Link className="Footer__textLink" to="/code-of-conduct">
-        Code of Conduct
-      </Link>
-      <span className="Footer__textSeparator">/</span>
-      <a
-        className="Footer__textLink"
-        href="https://www.unfinishedlabs.io"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Unfinished Labs
-      </a>
+      <div className="Footer__width" data-aos-offset="0" data-aos="fade">
+        <img src={Logo} />
+        <div className="Footer__column">
+          <div className="Footer__column">
+            <a
+              className="Footer__textLink"
+              href="mailto:hello@projectliberty.io"
+            >
+              Contact
+            </a>
+            <a
+              className="Footer__textLink"
+              href="https://www.unfinishedlabs.io"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Unfinished Labs
+            </a>
+            <a className="Footer__textLink">
+              About Project Liberty and its Partners
+            </a>
+          </div>
+          <br />
+          <div>Website last updated: 12/34/56 ©2020 Liberty 3.0 LLC</div>
+        </div>
+        <div className="Footer__column">
+          <Link className="Footer__textLink" to="/privacy-policy">
+            Privacy Policy
+          </Link>
+          <Link className="Footer__textLink" to="/code-of-conduct">
+            Code of Conduct
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
