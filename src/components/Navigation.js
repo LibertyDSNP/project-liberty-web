65173d1b86a2a7feaa0e60fe1110bb4d11e5b6da
V. Claire Olmstead <43625033+claireolmstead@users.noreply.github.com>
2021-03-25 15:27:55 -0700

    mobile nav styling (#17)
    * mobile nav styling
    
    * make bars smaller for mobile
    
    * change classname to BEM

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,30 +1,83 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Navigation = () => {
+  const [showNav, setShowNav] = React.useState(false);
+  const [navIconClassName, setNavIconClassName] = React.useState(
+    "Navigation__burger"
+  );
+
+  useEffect(() => {
+    if (showNav) {
+      setNavIconClassName("Navigation__burger Navigation__exitIcon");
+    } else {
+      setNavIconClassName("Navigation__burger");
+    }
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > 970) {
+        setShowNav(true);
+      } else {
+        setShowNav(false);
+      }
+    });
+  }, [showNav]);
+
+  const hideDropdownNav = () => {
+    if (window.innerWidth < 971) {
+      setShowNav(false);
+    }
+  };
+
   return (
-    <div className="Navigation__block">
-      <Link to="/about" className="Navigation__link">
-        About
-      </Link>
-      <span className="Navigation__divider"> / </span>
-      <Link to="/developer-portal" className="Navigation__link">
-        Developer Portal
-      </Link>
-      <span className="Navigation__divider"> / </span>
-      <Link to="/lets-connect" className="Navigation__link">
-        Let's Connect
-      </Link>
-      <span className="Navigation__divider"> / </span>
-      <a
-        className="Navigation__link"
-        href="https://github.com/LibertyDSNP/papers"
-        target="_blank"
-        rel="noreferrer"
+    <>
+      <div
+        className={navIconClassName}
+        onClick={() => {
+          setShowNav(!showNav);
+        }}
       >
-        Whitepaper
-      </a>
-    </div>
+        <div className="Navigation__burger--bar1"></div>
+        <div className="Navigation__burger--bar2"></div>
+        <div className="Navigation__burger--bar3"></div>
+      </div>
+      {showNav && (
+        <div className="Navigation__block">
+          <Link
+            to="/about"
+            className="Navigation__link"
+            onClick={() => hideDropdownNav()}
+          >
+            About
+          </Link>
+          <span className="Navigation__divider"> / </span>
+          <Link
+            to="/developer-portal"
+            className="Navigation__link"
+            onClick={() => hideDropdownNav()}
+          >
+            Developer Portal
+          </Link>
+          <span className="Navigation__divider"> / </span>
+          <Link
+            to="/lets-connect"
+            className="Navigation__link"
+            onClick={() => hideDropdownNav()}
+          >
+            Let's Connect
+          </Link>
+          <span className="Navigation__divider"> / </span>
+          <a
+            className="Navigation__link showMenu"
+            href="https://github.com/LibertyDSNP/papers"
+            target="_blank"
+            rel="noreferrer"
+            onClick={() => hideDropdownNav()}
+          >
+            Whitepaper
+          </a>
+        </div>
+      )}
+    </>
   );
 };
 
